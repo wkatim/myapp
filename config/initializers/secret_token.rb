07e68493a91c35c3a533c6b533287bec4f86989f
katim <wkatim@gmail.com>
2015-08-11 15:08:54 +0800

    a

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myaspp::Application.config.secret_token = '6fdd93ece2cd5e4848966b21c1b6d076084c2a5c0ee626b9cef5ea3342ac93fda2b10a341cd556d333e06867f5f51c8fb9fba6f1ef464bc821fa91ccb11bfa34'
